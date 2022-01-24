7af698393e75010e88fef015ea859e41382ad99f
Allan Nesathurai <ahnesathurai@gmail.com>
2022-01-23 20:40:05 -0500

    avx created and printing, tentatively fixed seg fault on case 11x11

--- project1/testing.cpp
+++ project1/testing.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <iomanip> // setw
 #include <stdio.h>
 #include <stdlib.h>
 #include<time.h>
@@ -37,8 +38,8 @@ float get_float(){
 }
 
 void print_float(vector<vector<float>> & in_matrix){
-    for(int i = 0; i < in_matrix.size(); i++){
-        for(int j = 0; j < in_matrix.size(); j++){
+    for(uint i = 0; i < in_matrix.size(); i++){
+        for(uint j = 0; j < in_matrix.size(); j++){
             cout << in_matrix[i][j] << " ";
         }
         cout << endl;
@@ -47,39 +48,42 @@ void print_float(vector<vector<float>> & in_matrix){
 };
 
 void print_float_avx(vector<vector<__m256>> & in_matrix_avx){
+    cout << left;
     cout << "in print avx fn " << endl;
     cout << endl;
-    int matrix_size = in_matrix_avx.size();
-    int num_of_regs = floor(matrix_size/8) + 1;
-    for(int vert = 0; vert < num_of_regs; vert++){
+    uint matrix_size = in_matrix_avx.size();
+    // divide by 8.0 to convert to float
+    uint num_of_regs = ceil(matrix_size/8.0);
+    for(uint vert = 0; vert < matrix_size; vert++){
         cout << endl;
         //vector<__m256> temp_row;
-        for(int horiz = 0; horiz < num_of_regs; horiz++){
+        for(uint horiz = 0; horiz < num_of_regs; horiz++){
             // looping through each element in register and checking if it exists
-            cout << "vert " << vert << " horiz " << horiz << endl; 
+            // cout << "vert " << vert << " horiz " << horiz; 
             // set the temporary register with 0s 
             //__m256 temp_reg = _mm256_set_ps(0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0);
-            for(int reg_i = 0; reg_i < 8; reg_i++){
-                cout << in_matrix_avx[horiz][vert][reg_i] << " ";
+            for(uint reg_i = 0; reg_i < 8; reg_i++){
+                cout << setprecision(2) << setw(8) << in_matrix_avx[vert][horiz][reg_i];
             }
             //cout << endl;
             //temp_row.push_back(temp_reg);
         };
+        // cout << endl;
         //in_matrix_avx.push_back(temp_row);
     };
 };
 
 void cpp_multiply( vector<vector<float>> & in_matrix, vector<vector<float>> & out_matrix ){
     // only works for square matrices  
-    for(int i = 0; i < in_matrix.size();i++){
-        for(int j = 0; j < in_matrix.size(); j++){
+    for(uint i = 0; i < in_matrix.size();i++){
+        for(uint j = 0; j < in_matrix.size(); j++){
 
             // grab left matrix row; in_matrix[i][1]
             // grab right matrix column; in_matrix[1][j]
             // multiply each value and add; so in_matrix[i][1]*in_matrix[1][j]
             // then in_matrix[i][2]*in_matrix[2][j] etc. 
             float temp = 0; 
-            for(int x = 0; x < in_matrix.size(); x++){
+            for(uint x = 0; x < in_matrix.size(); x++){
                 temp += in_matrix[i][x]*in_matrix[x][j];
             }
             out_matrix[i][j] = temp;
@@ -89,22 +93,22 @@ void cpp_multiply( vector<vector<float>> & in_matrix, vector<vector<float>> & ou
 
 void avx_multiply( vector<vector<float>> & in_matrix, vector<vector<float>> & out_matrix ){
     // only works for square matrices  
-    for(int i = 0; i < in_matrix.size();i++){
-        for(int j = 0; j < in_matrix.size(); j++){
+    for(uint i = 0; i < in_matrix.size();i++){
+        for(uint j = 0; j < in_matrix.size(); j++){
 
             // grab left matrix row; in_matrix[i][1]
             // grab right matrix column; in_matrix[1][j]
             // multiply each value and add; so in_matrix[i][1]*in_matrix[1][j]
             // then in_matrix[i][2]*in_matrix[2][j] etc. 
             float temp = 0; 
-            for(int x = 0; x < in_matrix.size(); x++){
+            for(uint x = 0; x < in_matrix.size(); x++){
                 //temp += _mm256_mul_ps(in_matrix[i][x],in_matrix[x][j]);
             }
             out_matrix[i][j] = temp;
         }
     }
 }
-
+// getting weird seg fault on certain values. like 11x11
 int main(int argc, char* argv[]){
     srand(1);
     // file input scheme from cs1200
@@ -121,11 +125,11 @@ int main(int argc, char* argv[]){
         exit(true);
     }
     
-    int matrix_size;
+    uint matrix_size;
     inputs >> matrix_size; // read in first value in inputs
     string numtype; 
     inputs >> numtype;
-    int numsize;
+    uint numsize;
     inputs >> numsize;
 
     cout<<"Inputs Accepted!"<<endl;
@@ -138,10 +142,10 @@ int main(int argc, char* argv[]){
     vector<vector<__m256>> out_matrix_avx;
     if( (numtype == "float") && (numsize == 4) ){
         // construct float matrix 
-        for(int i = 0; i < matrix_size; i++){
+        for(uint i = 0; i < matrix_size; i++){
             vector<float> temp;
             vector<float> temp2;
-            for(int j = 0; j < matrix_size; j++){
+            for(uint j = 0; j < matrix_size; j++){
                 temp.push_back(get_float());
                 temp2.push_back(0);
             }
@@ -151,21 +155,28 @@ int main(int argc, char* argv[]){
         print_float(in_matrix);
         // construct __m256 matrix
         // need to pad with 0's at the end if not perfectly divisable 
-        int num_of_regs = floor(matrix_size/8) + 1;
+        
+        // divide by 8.0 to convert to float 
+        uint num_of_regs = ceil(matrix_size/8.0);
         //cout << num_of_regs << endl;
         // looping through the required registers 
         // [(x x x x x x x x) (x x x x x x x x)] <- two 8 float registers | two vectors down 
         // [(x x x x x x x x) (x x x x x x x x)]                          |
         //                  ^ one vectotr wide 
-        for(int vert = 0; vert < matrix_size; vert++){
+        //vector<__m256> temp_row;
+        //__m256 temp_reg = _mm256_set_ps(0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0);
+        //temp_row.push_back(temp_reg)
+        for(uint vert = 0; vert < matrix_size; vert++){
+            //temp_row.clear();
             vector<__m256> temp_row;
-            for(int horiz = 0; horiz < num_of_regs; horiz++){
+            for(uint horiz = 0; horiz < num_of_regs; horiz++){
                 // looping through each element in register and checking if it exists
                 cout << "vert " << vert << " horiz " << horiz << endl; 
                 // set the temporary register with 0s 
+                /*
                 __m256 temp_reg = _mm256_set_ps(0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0);
-                for(int reg_i = 0; reg_i < 8; reg_i++){
-                    int real_i = horiz*8 + reg_i;
+                for(uint reg_i = 0; reg_i < 8; reg_i++){
+                    uint real_i = horiz*8 + reg_i;
                     cout << real_i << " ";
                     if(real_i < matrix_size){
                         temp_reg[reg_i] = in_matrix[vert][real_i];
@@ -173,7 +184,23 @@ int main(int argc, char* argv[]){
                     else{
                         temp_reg[reg_i] = 0.0;
                     }
+                }*/
+                
+                vector<float> temp_vector;
+                for(uint reg_i = 0; reg_i < 8; reg_i++){
+                    uint real_i = horiz*8 + reg_i;
+                    cout << real_i << " ";
+                    if(real_i < matrix_size){
+                        //temp_reg[reg_i] = in_matrix[vert][real_i];
+                        temp_vector.push_back(in_matrix[vert][real_i]);
+                    }
+                    else{
+                        temp_vector.push_back(0);
+                        //temp_reg[reg_i] = 0.0;
+                    }
                 }
+                __m256 temp_reg = _mm256_set_ps(temp_vector[7],temp_vector[6],temp_vector[5],temp_vector[4],
+                                                temp_vector[3],temp_vector[2],temp_vector[1],temp_vector[0]);
                 cout << endl;
                 temp_row.push_back(temp_reg);
             };
