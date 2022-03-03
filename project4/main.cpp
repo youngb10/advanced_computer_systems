72d3106e33df2d425fb691843dc996ae2c9e79eb
Allan Nesathurai <ahnesathurai@gmail.com>
2022-03-03 16:16:07 -0500

    mostly done implementation. needs smallish changes

--- project4/main.cpp
+++ project4/main.cpp
@@ -12,30 +12,125 @@
 #include <string.h>
 #include <sys/resource.h>
 #include <cstddef>         // std::size_t
+#include <unordered_map>
 
-using namespace std;
+//namespace fs = std::filesystem;
 using std::chrono::high_resolution_clock;
 using std::chrono::time_point;
 using std::chrono::duration;
 
+float get_size(std::string file){
+    // https://www.tutorialspoint.com/how-can-i-get-a-file-s-size-in-cplusplus
+    std::ifstream in_file(file, std::ios::binary);
+    in_file.seekg(0, std::ios::end);
+    float file_size = in_file.tellg();
+    return file_size;
+}
 
+void print_dict(std::unordered_map<std::size_t, std::string> &umap){
+    // may want to make a slower sorted version
+    std::unordered_map<std::size_t, std::string>::iterator itr;
+    for (itr = umap.begin(); itr != umap.end(); itr++)
+    {
+        // itr works as a pointer to pair<string, double>
+        // type itr->first stores the key part  and
+        // itr->second stores the value part
+        std::cout << itr->first << "  " << itr->second << std::endl;
+    }
+}
 
-int main(int argc, char *argv[])
-{
-    // processing input parameters
+void write_dict(std::unordered_map<std::size_t, std::string> &umap, std::string &out_file, int debug){
+    // output is reopened and will append to end of file
+    // may want to make a slower sorted version
+    // https://www.delftstack.com/howto/cpp/how-to-append-text-to-a-file-in-cpp/
     
-    ifstream params(argv[1]); // input file
+    std::ofstream out;
+    out.open(out_file, std::ios_base::app);
+    out << "__dictbegin__" << std::endl;
+    std::unordered_map<std::size_t, std::string>::iterator itr;
+    for (itr = umap.begin(); itr != umap.end(); itr++)
+    {
+        // itr works as a pointer to pair<string, double>
+        // type itr->first stores the key part  and
+        // itr->second stores the value part
+        out << itr->first << ";" << itr->second << std::endl;
+    }
+    out.close();
+    if(debug){std::cout << "Dictionary writing has completed!" << std::endl;}
+}
+
+void parse_dict(std::unordered_map<std::size_t, std::string> &umap, std::string &compressed_file){
+    // want to skip to the beginning of the dictionary then create the dictionary 
+    std::ifstream compressed(compressed_file);
+    std::string line;
+    
+    std::string cword;
+    std::string ckey_str;
+    std::size_t ckey;
+    int found = 0; 
+    while(!compressed.eof()){
+        std::getline(compressed,line);
+        // the dictionary has started
+        if(line == "__dictbegin__"){
+            found = 1;
+        }
+        if(found){
+            // split by semicolon delineator 
+            // write to dictionary; left of find is key, right is value
+            std::size_t found = line.find_last_of(";");
+            ckey_str = line.substr(0,found);
+            int ckey_int = std::stoi(ckey_str);
+            ckey = ckey_int;
+            cword = line.substr(found+1);
+            umap.insert({ ckey, cword });
+        }
+    }
+    compressed.close();
+}
+
+int is_equal(std::string &input_file, std::string &decompressed_file){
+
+    // open both files in read mode
+    std::ifstream input(input_file);
+    std::ifstream decompressed(decompressed_file);
+
+    std::string cword;
+    std::string dword;
+    // files SHOULD be equal length... if not then bad
+    // may need to implement size checking
+    while(!input.eof()){
+        getline(input,cword);
+        getline(decompressed,dword);
+        if((cword != "") && (dword != "")){
+            if(cword != dword){
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+void check_file(std::ifstream &file, std::string &file_name){
     // check that the parameters are loaded
-    if (!params.good())
+    if (!file.good())
     { // pulled from submitty resources, cs1200, on helpful c++ programming information
       // https://www.cs.rpi.edu/academics/courses/spring20/csci1200/programming_information.php
-        std::cerr << "Can't open the parameters.txt file: " << argv[1] << " ; please try again" << std::endl;
+        std::cerr << "Can't open the file: " << file_name << " ; please try again" << std::endl;
         exit(true);
     }
-    
-    int debug;
-    std::string input_file;
-    std::string output_file;
+}
+
+void check_file(std::ofstream &file, std::string &file_name){
+    // check that the parameters are loaded
+    if (!file.good())
+    { // pulled from submitty resources, cs1200, on helpful c++ programming information
+      // https://www.cs.rpi.edu/academics/courses/spring20/csci1200/programming_information.php
+        std::cerr << "Can't open the file: " << file_name << " ; please try again" << std::endl;
+        exit(true);
+    }
+}
+
+void construct_params(std::ifstream &params, int &debug, std::string &input_file, std::string &output_file){
     
     std::string line;
     getline(params,line);
@@ -45,38 +140,184 @@ int main(int argc, char *argv[])
         std::cerr << "Wrong input for debug; must be either 0 (disabled) or 1 (enabled); recieved value: " << debug << std::endl;
         std::cout << "Wrong input for debug; must be either 0 (disabled) or 1 (enabled); recieved value: " << debug << std::endl;
     }
-    if(debug == 1) std::cout << line << std::endl;
-
+    
     getline(params,line);
     getline(params,line);
-    if(debug == 1) std::cout << line << std::endl;
     input_file = line;
-    ifstream input(input_file); // input file
-    // check that the parameters are loaded
-    if (!input.good())
-    { // pulled from submitty resources on helpful c++ programming information
-        std::cerr << "Can't open the inputs.txt file: " << input_file << " ; please try again" << std::endl;
-        exit(true);
-    }
-
+    
     std::string output_prefix = "compressed_";
     //https://www.cplusplus.com/reference/string/string/find_last_of/
     std::size_t found = input_file.find_last_of("/\\");
-    output_file = input_file.substr(0,found) + output_prefix + input_file.substr(found+1);
-    ofstream output(output_file); // output file
-    if (!output.good())
-    { // pulled from submitty resources on helpful c++ programming information
-        std::cerr << "Can't open the output.txt file: " << output_file << " ; please try again" << std::endl;
-        exit(true);
+    output_file = input_file.substr(0,found) + "/" + output_prefix + input_file.substr(found+1);
+
+    if(debug == 1) std::cout << "Debug: " << debug << std::endl;
+    std::cout << "Input File: " << input_file << std::endl;
+    std::cout << "  Size in bytes: " << get_size(input_file) << std::endl;
+    std::cout << "Output File: " << output_file << std::endl;
+}
+
+void construct_decompress_output_file(std::string &input_file, std::string &decomp_output_file){
+    // create decompression output file 
+    std::string decomp_output_prefix = "decompressed_";
+    //https://www.cplusplus.com/reference/string/string/find_last_of/
+    std::size_t decomp_found = input_file.find_last_of("/\\");
+    decomp_output_file = input_file.substr(0,decomp_found) + "/" + decomp_output_prefix + input_file.substr(decomp_found+1);
+}
+
+void compress(std::ifstream &input, std::ofstream &output, std::unordered_map<std::size_t, std::string> &dict, int debug){
+    // compress file and construct dictionary 
+    std::size_t dict_key = 0;
+    std::string word;
+    std::size_t key;
+    while(!input.eof()){
+        // read in value
+        getline(input,word);
+        if(word != ""){
+            int found = 0;
+            for (std::unordered_map<std::size_t, std::string>::iterator itr = dict.begin(); itr != dict.end(); ++itr) {
+                if (itr->second == word){
+                    key = itr->first;
+                    found = 1;
+                }
+            }
+            if(!found){
+                // if not found, add key to dict
+                dict.insert(make_pair(dict_key,word));   
+                key = dict_key;
+                dict_key += 1;
+            }
+            // now, create output 
+            output << key << std::endl;
+            if(debug){std::cout << "Key: " << key << " Value: " << word << std::endl;}
+        }
     }
+    if(debug){print_dict(dict);}
+}
 
-    std::cout << "Input parameters accepted" << std::endl;
+void decompress(std::ifstream &compressed, std::ofstream &decompressed, std::unordered_map<std::size_t, std::string> &dict, int debug){
+    // decompress compressed file and write output to decompressed
+    // stop decompressing once the dictionary has been hit
+    std::string ckey_str;
+    std::size_t ckey;
+    int end = 0;
+    while(!compressed.eof()){
+        // read in value
+        getline(compressed,ckey_str);
+        if(ckey_str == "__dictbegin__"){
+            end = 1;
+        }
+        // if key not empty, and not at the end 
+        if( (ckey_str != "") && (end == 0)){
+            // read input string, convert to int then cast to ckey for size_t
+            int ckey_int = std::stoi(ckey_str);
+            ckey = ckey_int;
+            //std::cout << word << std::endl;
+            // search dictionary for key
+            // https://www.cplusplus.com/reference/map/map/find/
+            
+            int found = 0;
+            std::unordered_map<std::size_t, std::string>::iterator citr;
+            citr = dict.find(ckey);
+            if(debug){std::cout << "Key: " << ckey << " Value: " << citr->second << std::endl;}
+            // if key found
+            if(citr != dict.end()){
+                // want to output the value to the output file (decompressed)
+                decompressed << citr->second << std::endl;
+            }
+            // if key not found 
+            else{
+                std::cerr << "decompression failed; no value for the key: " << ckey << std::endl;
+            }
+        }
+    }
+}
+
+using std::chrono::high_resolution_clock;
+using std::chrono::time_point;
+using std::chrono::system_clock;
+using std::chrono::duration;
+high_resolution_clock::time_point start_time = high_resolution_clock::now();
+int time_first = 1;
+
+void delta(std::string msg = ""){
+    //duration<double, std::milli> t2 = (end2 - start2) / 1000;
+    duration<double, std::milli> del;
+    //time_point<system_clock,duration<double>> zero_{};
+
+    if(time_first){
+        start_time = high_resolution_clock::now();
+        time_first = 0;
+        return;
+    }
+    del = (high_resolution_clock::now() - start_time);
+    std::cout << msg << del.count() << std::endl;
+    start_time = high_resolution_clock::now();
+}
+
+int main(int argc, char *argv[])
+{
+    delta();
+    // check input parameters and open input file 
+    std::ifstream params(argv[1]); // input file
+    std::string params_file = argv[1];
+
+    check_file(params, params_file);
+    delta();
+    int debug;
+    std::string input_file;
+    std::string output_file;
+    construct_params(params, debug, input_file, output_file);
     
+    // open input and output, check that the operation succedded 
+    std::ifstream input(input_file); // input file
+    check_file(input,input_file);
+    std::ofstream output(output_file); // output file
+    check_file(output,output_file);
+    params.close();
+    
+    std::cout << "Input parameters accepted!" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Compression has started!" << std::endl;
     
 
-    
+    // https://www.geeksforgeeks.org/unordered_map-in-cpp-stl/
+    // will use this built in hash map. might reimplement with b tree later
+    std::unordered_map<std::size_t, std::string> dict;
 
+    // compress file and construct dictionary
+    // add optimization where most frequent words use smallest key 
+    compress(input, output, dict, debug);
+
+    // now, write dictionary to the end of the file 
+    input.close();
     output.close();
+    
+    write_dict(dict, output_file, debug);
+    
+    std::cout << "Compression has completed!" << std::endl;
+    std::cout << "Decompression has started!" << std::endl;
+    
+    // decompress 
+    // open the output as an input now
+    std::ifstream compressed(output_file); 
+    // create decompressed file output name
+    std::string decomp_output_file;
+    construct_decompress_output_file(input_file,decomp_output_file);
+    // open the output file
+    std::ofstream decompressed(decomp_output_file); // output file
+    // run the actual decompression algorithm
+    decompress(compressed, decompressed, dict, debug);
+
+    compressed.close();
+    decompressed.close();
+    std::cout << "Deompression has completed!" << std::endl;
+
+    // check if compression and decompression was successful
+    int passed = is_equal(decomp_output_file,input_file);
+    std::cout << "Original file and compressed file are identical: " << passed << std::endl;
+    
+    // add statistics based on compression ratio 
+    std::cout << "Compression ratio (larger over smaller): " << get_size(input_file) << "/" << get_size(output_file);
+    std::cout << "\n    = " << get_size(input_file) / get_size(output_file) << std::endl;
     return 0;
 };
-
