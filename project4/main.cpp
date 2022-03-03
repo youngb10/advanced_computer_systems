13a5f89bff6b9df9cf21b7ba4f66d23a9f8bc8f5
Allan Nesathurai <ahnesathurai@gmail.com>
2022-03-03 18:10:14 -0500

    working project4. implemented frequency dict.

--- project4/main.cpp
+++ project4/main.cpp
@@ -13,11 +13,52 @@
 #include <sys/resource.h>
 #include <cstddef>         // std::size_t
 #include <unordered_map>
+#include <algorithm>
 
 //namespace fs = std::filesystem;
 using std::chrono::high_resolution_clock;
 using std::chrono::time_point;
 using std::chrono::duration;
+
+duration<double, std::milli> delta(std::string msg);
+float get_size(std::string file);
+void print_dict(std::unordered_map<std::size_t, std::string> &umap);
+void print_dict(std::unordered_map<std::string, std::size_t> &umap);
+void write_dict(std::unordered_map<std::size_t, std::string> &umap, std::string &out_file, int debug);
+void parse_dict(std::unordered_map<std::size_t, std::string> &umap, std::string &compressed_file);
+int is_equal(std::string &input_file, std::string &decompressed_file);
+void check_file(std::ifstream &file, std::string &file_name);
+void check_file(std::ofstream &file, std::string &file_name);
+void construct_params(std::ifstream &params, int &debug, std::string &input_file, std::string &output_file);
+void construct_decompress_output_file(std::string &input_file, std::string &decomp_output_file);
+void compress(std::ifstream &input, std::ofstream &output, std::unordered_map<std::size_t, std::string> &dict, int debug);
+void decompress(std::ifstream &compressed, std::ofstream &decompressed, std::unordered_map<std::size_t, std::string> &dict, int debug);
+
+using std::chrono::high_resolution_clock;
+using std::chrono::time_point;
+using std::chrono::system_clock;
+using std::chrono::duration;
+high_resolution_clock::time_point start_time = high_resolution_clock::now();
+int time_first = 1;
+
+duration<double, std::milli> delta(std::string msg = ""){
+    //duration<double, std::milli> t2 = (end2 - start2) / 1000;
+    duration<double, std::milli> del;
+    //time_point<system_clock,duration<double>> zero_{};
+    int silent = 0;
+    if(msg == ""){silent = 1;}
+    if(time_first){
+        start_time = high_resolution_clock::now();
+        time_first = 0;
+        del = (high_resolution_clock::now() - high_resolution_clock::now()) / 1000;
+    }
+    else{
+        del = (high_resolution_clock::now() - start_time) / 1000;
+        if(!silent){std::cout << "  " << msg << ": " << del.count() << " s" << std::endl;}
+        start_time = high_resolution_clock::now();
+    }
+    return del;
+}
 
 float get_size(std::string file){
     // https://www.tutorialspoint.com/how-can-i-get-a-file-s-size-in-cplusplus
@@ -31,6 +72,30 @@ void print_dict(std::unordered_map<std::size_t, std::string> &umap){
     // may want to make a slower sorted version
     std::unordered_map<std::size_t, std::string>::iterator itr;
     for (itr = umap.begin(); itr != umap.end(); itr++)
+    {
+        // itr works as a pointer to pair<string, double>
+        // type itr->first stores the key part  and
+        // itr->second stores the value part
+        std::cout << itr->first << "  " << itr->second << std::endl;
+    }
+}
+
+void print_dict(std::unordered_map<std::string, std::size_t> &umap){
+    // may want to make a slower sorted version
+    std::unordered_map<std::string, std::size_t>::iterator itr;
+    for (itr = umap.begin(); itr != umap.end(); itr++)
+    {
+        // itr works as a pointer to pair<string, double>
+        // type itr->first stores the key part  and
+        // itr->second stores the value part
+        std::cout << itr->first << "  " << itr->second << std::endl;
+    }
+}
+
+void print_vec(std::vector<std::pair<std::string, size_t>> &pair_vec){
+    // may want to make a slower sorted version
+    std::vector<std::pair<std::string, size_t>>::iterator itr;
+    for (itr = pair_vec.begin(); itr != pair_vec.end(); itr++)
     {
         // itr works as a pointer to pair<string, double>
         // type itr->first stores the key part  and
@@ -169,27 +234,73 @@ void compress(std::ifstream &input, std::ofstream &output, std::unordered_map<st
     std::size_t dict_key = 0;
     std::string word;
     std::size_t key;
+    std::unordered_map<std::string, std::size_t> dict_rev;
+    //duration<double, std::milli> find_time;
+    //duration<double, std::milli> insert_time;
     while(!input.eof()){
         // read in value
         getline(input,word);
         if(word != ""){
             int found = 0;
-            for (std::unordered_map<std::size_t, std::string>::iterator itr = dict.begin(); itr != dict.end(); ++itr) {
-                if (itr->second == word){
-                    key = itr->first;
-                    found = 1;
-                }
+            //delta("  compression; time to find key");
+            //find_time = delta();
+            
+            // using a reverse map to search for values since iterating is extremely slow
+            std::unordered_map<std::string, std::size_t>::iterator citr;
+            citr = dict_rev.find(word);
+            if(citr != dict_rev.end()){
+                found = 1;
+                key = citr->second;
             }
+
             if(!found){
                 // if not found, add key to dict
+                //insert_time = delta();
                 dict.insert(make_pair(dict_key,word));   
+                dict_rev.insert(make_pair(word,dict_key));
+                //delta("  compression; time to insert key pair");
                 key = dict_key;
                 dict_key += 1;
             }
             // now, create output 
             output << key << std::endl;
             if(debug){std::cout << "Key: " << key << " Value: " << word << std::endl;}
+        
         }
+        //std::cout << find_time.count() / insert_time.count() << std::endl;
+        //delta("  compression; time to compress line");
+    }
+    if(debug){print_dict(dict);}
+}
+
+void compress_better(std::ifstream &input, std::ofstream &output, std::unordered_map<std::size_t, std::string> &dict, std::unordered_map<std::string, std::size_t> &dict_rev, int debug){
+    // compress file and construct dictionary 
+    
+    std::string word;
+    std::size_t key;
+
+    // take dictionary, then assign new values
+    
+    //duration<double, std::milli> find_time;
+    //duration<double, std::milli> insert_time;
+    std::string ckey_str;
+    std::size_t ckey;
+    while(!input.eof()){
+        // read in value
+        getline(input,word);
+        if(word != ""){      
+            //delta("  compression; time to find key");
+            //find_time = delta();
+            
+            // find value using key
+            std::unordered_map<std::string, std::size_t>::iterator citr;
+            citr = dict_rev.find(word);
+            // now, create output 
+            output << citr->second << std::endl;
+            if(debug){std::cout << "Key: " << key << " Value: " << word << std::endl;}
+        }
+        //std::cout << find_time.count() / insert_time.count() << std::endl;
+        //delta("  compression; time to compress line");
     }
     if(debug){print_dict(dict);}
 }
@@ -232,26 +343,59 @@ void decompress(std::ifstream &compressed, std::ofstream &decompressed, std::uno
     }
 }
 
-using std::chrono::high_resolution_clock;
-using std::chrono::time_point;
-using std::chrono::system_clock;
-using std::chrono::duration;
-high_resolution_clock::time_point start_time = high_resolution_clock::now();
-int time_first = 1;
+bool comp(std::pair<std::string, std::size_t> a, std::pair<std::string, std::size_t> b) {
+    return a.second > b.second;
+}
 
-void delta(std::string msg = ""){
-    //duration<double, std::milli> t2 = (end2 - start2) / 1000;
-    duration<double, std::milli> del;
-    //time_point<system_clock,duration<double>> zero_{};
+void construct_dict_better(std::string input_file, std::unordered_map<std::size_t, std::string> &dict, std::unordered_map<std::string, std::size_t> &dict_rev, int debug){
+    std::unordered_map<std::string, std::size_t> dict_rev_freq;
+    std::ifstream input(input_file);
+    // go through entire file 
+    // compress file and construct dictionary 
+    std::string word;
 
-    if(time_first){
-        start_time = high_resolution_clock::now();
-        time_first = 0;
-        return;
+    while(!input.eof()){
+        // read in value
+        getline(input,word);
+        if(word != ""){
+            int found = 0;
+            //delta("  compression; time to find key");
+            //find_time = delta();
+            
+            // using a reverse map to search for values since iterating is extremely slow
+            std::unordered_map<std::string, std::size_t>::iterator citr;
+            citr = dict_rev_freq.find(word);
+            if(citr != dict_rev_freq.end()){
+                found = 1;
+                citr->second += 1;
+            }
+            if(!found){
+                // if not found, add key to dict
+                dict_rev_freq.insert(make_pair(word,0));
+            }
+        }
+        //std::cout << find_time.count() / insert_time.count() << std::endl;
+        //delta("  compression; time to compress line");
     }
-    del = (high_resolution_clock::now() - start_time);
-    std::cout << msg << del.count() << std::endl;
-    start_time = high_resolution_clock::now();
+    //print_dict(dict_rev_freq);
+    // https://stackoverflow.com/questions/31323135/sort-an-unordered-map-using-sort
+    std::vector<std::pair<std::string, size_t>> sorted_pair(dict_rev_freq.begin(), dict_rev_freq.end());
+    sort(sorted_pair.begin(), sorted_pair.end(), comp);
+    //print_vec(sorted_pair);
+    input.close();
+
+    // construct dictionary with low to high frequency corresponding to large to small numbers
+    std::size_t dict_key = 0;
+
+    std::vector<std::pair<std::string, size_t>>::iterator itr;
+    for (itr = sorted_pair.begin(); itr != sorted_pair.end(); itr++)
+    {
+        dict.insert(make_pair(dict_key,itr->first));
+        dict_rev.insert(make_pair(itr->first,dict_key));
+        dict_key += 1;
+    }
+    //print_dict(dict);
+
 }
 
 int main(int argc, char *argv[])
@@ -262,7 +406,7 @@ int main(int argc, char *argv[])
     std::string params_file = argv[1];
 
     check_file(params, params_file);
-    delta();
+    delta("time to check files");
     int debug;
     std::string input_file;
     std::string output_file;
@@ -276,25 +420,30 @@ int main(int argc, char *argv[])
     params.close();
     
     std::cout << "Input parameters accepted!" << std::endl;
+    delta("time to construct parameters");
     std::cout << std::endl;
     std::cout << "Compression has started!" << std::endl;
-    
 
     // https://www.geeksforgeeks.org/unordered_map-in-cpp-stl/
     // will use this built in hash map. might reimplement with b tree later
     std::unordered_map<std::size_t, std::string> dict;
+    std::unordered_map<std::string, std::size_t> dict_rev;
 
     // compress file and construct dictionary
     // add optimization where most frequent words use smallest key 
-    compress(input, output, dict, debug);
-
-    // now, write dictionary to the end of the file 
+    construct_dict_better(input_file, dict, dict_rev, debug);
+    compress_better(input, output, dict, dict_rev, debug);
+    //print_dict(dict);
+    // compress(input, output, dict, debug);
     input.close();
     output.close();
-    
+    delta("time to compress");
+
+    // now, write dictionary to the end of the file 
     write_dict(dict, output_file, debug);
-    
+    delta("time to write dictionary");
     std::cout << "Compression has completed!" << std::endl;
+    std::cout << std::endl;
     std::cout << "Decompression has started!" << std::endl;
     
     // decompress 
@@ -311,11 +460,13 @@ int main(int argc, char *argv[])
     compressed.close();
     decompressed.close();
     std::cout << "Deompression has completed!" << std::endl;
-
+    delta("time to decompress");
+    std::cout << std::endl;
     // check if compression and decompression was successful
     int passed = is_equal(decomp_output_file,input_file);
     std::cout << "Original file and compressed file are identical: " << passed << std::endl;
-    
+    delta("time to check that the files are identical");
+
     // add statistics based on compression ratio 
     std::cout << "Compression ratio (larger over smaller): " << get_size(input_file) << "/" << get_size(output_file);
     std::cout << "\n    = " << get_size(input_file) / get_size(output_file) << std::endl;
